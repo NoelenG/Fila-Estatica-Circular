cdd63c5b665a57910bd64756700dda92e5c19edd
EmersonPaduan <emerson.paduan@gmail.com>
2020-03-11 08:15:43 -0300

    commit estrutura inicial

---
+++ src/main/java/br/paduan/App.java
@@ -0,0 +1,10 @@
+package br.paduan;
+
+public final class App {
+    private App() {
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Exemplo de aplicação.");
+    }
+}
---
+++ src/main/java/br/paduan/Queue.java
@@ -0,0 +1,12 @@
+package br.paduan;
+
+public class Queue {
+    private int data[];
+    int end;
+
+    public Queue(int size){
+        data = new int[size];
+        end = -1;
+    }
+
+}
